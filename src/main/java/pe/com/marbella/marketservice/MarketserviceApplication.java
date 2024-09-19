8e77c5ee405523c54349d57c129b0653b325800a
Rosa Izquierdo <i202212506@cibertec.edu.pe>
2024-09-18 20:38:04 -0500

    Commit inicial de proyecto backend

---
+++ src/main/java/pe/com/marbella/marketservice/MarketserviceApplication.java
@@ -0,0 +1,13 @@
+package pe.com.marbella.marketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/com/marbella/marketservice/MarketserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.marbella.marketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
