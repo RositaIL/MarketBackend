ef064f5ffaa7bdb48b2619ca3a016bf044007b76
GuilleCalM <guilleacalderon@hotmail.com>
2024-10-06 12:25:18 -0500

    Corrección de manejo de groups de validación en UsuarioDTO

--- src/main/java/pe/com/marbella/marketservice/dto/UsuarioDTO.java
+++ src/main/java/pe/com/marbella/marketservice/dto/UsuarioDTO.java
@@ -11,16 +11,16 @@ public record UsuarioDTO(
         @NotNull(groups = OnUpdate.class)Long idUsuario,
 
         @NotEmpty(groups = OnCreate.class,message = "- Debe especificar el nombre completo del usuario")
-        @Size(groups = OnCreate.class,max = 50, message = "- El nombre completo no debe exceder los 50 caracteres")
+        @Size(groups = {OnCreate.class, OnUpdate.class},max = 50, message = "- El nombre completo no debe exceder los 50 caracteres")
         String nombresApellidosUsu,
 
         @NotEmpty(groups = OnCreate.class,message = "- Debe especificar el correo electrónico")
-        @Email(groups = OnCreate.class,message = "- Debe ser un correo electrónico válido")
-        @Size(groups = OnCreate.class,max = 50, message = "- El correo electrónico no debe exceder los 50 caracteres")
+        @Email(groups = {OnCreate.class, OnUpdate.class},message = "- Debe ser un correo electrónico válido")
+        @Size(groups = {OnCreate.class, OnUpdate.class},max = 50, message = "- El correo electrónico no debe exceder los 50 caracteres")
         String emailUsu,
 
         @NotEmpty(groups = OnCreate.class,message = "- Debe especificar el login de usuario")
-        @Size(groups = OnCreate.class,min = 5, max = 15, message = "- El login de usuario debe medir entre 5 y 15 caracteres")
+        @Size(groups = {OnCreate.class, OnUpdate.class},min = 5, max = 15, message = "- El login de usuario debe medir entre 5 y 15 caracteres")
         String username,
 
         @NotEmpty(groups = OnCreate.class,message = "- Debe especificar la contraseña")
